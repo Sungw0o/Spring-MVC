c14d7502da5ba36777568eb48a2850a1d36df83e
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-01 08:53:10 +0900

    Section 3

---
+++ src/test/java/hello/servlet/domain/member/MemberRepoistoryTest.java
@@ -0,0 +1,48 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepoistoryTest {
+
+    MemberRepoistory memberRepoistory = MemberRepoistory.getInstance();
+
+    @AfterEach
+    void afterEach(){
+        memberRepoistory.clearStore();
+    }
+    @DisplayName("저장 테스트")
+    @Test
+    void save(){
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member savedMember = memberRepoistory.save(member);
+        //then
+        Member findMember = memberRepoistory.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+    @DisplayName("리스트 출력")
+    @Test
+    void findAll(){
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepoistory.save(member1);
+        memberRepoistory.save(member2);
+        //when
+        List<Member> result = memberRepoistory.findAll();
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+
+}
